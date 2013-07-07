79713bc53c4b92b58b4d2901febf33556f458270
Christian Oudard <christian.oudard@gmail.com>
2013-07-06 19:31:45 -0700

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='vec',
+    version='0.1',
+    description='',
+    author='Christian Oudard',
+    author_email='christian.oudard@gmail.com',
+    url='',
+    packages=['vec'],
+)
